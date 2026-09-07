3392c72f3fbb6388c8ff8e9781cb45f7100a9823
YakeliJimenezV <yakeli.jimenez@tecsup.edu.pe>
2026-09-07 12:35:20 -0500

    Commit 1: Agregarr modelos de líneas y estaciones

--- semana03/ejercicio-estaciones/estaciones.playground/Contents.swift
+++ semana03/ejercicio-estaciones/estaciones.playground/Contents.swift
@@ -1,3 +1,94 @@
-import UIKit
+struct Estacion {
+    let nombre: String
+    let distrito: String
+    let avenida: String
+    let lugaresCercanos: [String]
+}
 
-var greeting = "Hello, playground"
+struct Linea {
+    let numero: Int
+    let nombre: String
+    let origen: String
+    let destino: String
+    let estado: String
+    let estaciones: [Estacion]
+}
+
+let atocongo = Estacion(
+    nombre: "Atocongo",
+    distrito: "San Juan de Miraflores",
+    avenida: "Av. Pedro Miotta",
+    lugaresCercanos: [
+        "Mall del Sur",
+        "TECSUP"
+    ]
+)
+
+print(atocongo.nombre)
+
+
+let linea1 = Linea(
+    numero: 1,
+    nombre: "Línea 1",
+    origen: "Villa El Salvador",
+    destino: "Bayóvar",
+    estado: "Operativa",
+    estaciones: [
+        Estacion(
+            nombre: "Villa El Salvador",
+            distrito: "Villa El Salvador",
+            avenida: "Av. Separadora Industrial",
+            lugaresCercanos: []
+        ),
+        Estacion(
+            nombre: "Parque Industrial",
+            distrito: "Villa El Salvador",
+            avenida: "Av. Separadora Industrial",
+            lugaresCercanos: []
+        ),
+        Estacion(
+            nombre: "Pumacahua",
+            distrito: "Villa María del Triunfo",
+            avenida: "Av. Unión",
+            lugaresCercanos: []
+        ),
+        Estacion(
+            nombre: "Villa María",
+            distrito: "Villa María del Triunfo",
+            avenida: "Av. Pachacútec",
+            lugaresCercanos: []
+        ),
+        Estacion(
+            nombre: "María Auxiliadora",
+            distrito: "San Juan de Miraflores",
+            avenida: "Av. Miguel Iglesias",
+            lugaresCercanos: []
+        ),
+        Estacion(
+            nombre: "San Juan",
+            distrito: "San Juan de Miraflores",
+            avenida: "Av. Los Héroes",
+            lugaresCercanos: []
+        ),
+        Estacion(
+            nombre: "Atocongo",
+            distrito: "San Juan de Miraflores",
+            avenida: "Av. Pedro Miotta",
+            lugaresCercanos: [
+                "Mall del Sur",
+                "TECSUP"
+            ]
+        )
+    ]
+)
+
+print("🚇 \(linea1.nombre)")
+print("Origen: \(linea1.origen)")
+print("Destino: \(linea1.destino)")
+print("Estado: \(linea1.estado)")
+
+print("\nEstaciones:")
+
+for estacion in linea1.estaciones {
+    print("- \(estacion.nombre)")
+}
